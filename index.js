3b26b1bf99bb098cf554604753da1a53a4c2e81a
mnin-nahid <nahidulislam.nayan54@gmail.com>
2022-05-28 17:45:56 +0600

    order delete operation success

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
+const port = process.env.PORT || 5000;
 
 const app = express();
-const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -15,24 +15,104 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.joyr7.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function varifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthorized Access' })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbideen access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 //function for all api
 async function run() {
     try {
         await client.connect();
         const productCollection = client.db('dihan_ltd').collection('products');
+        const orderCollection = client.db('dihan_ltd').collection('order');
+        const userCollection = client.db('dihan_ltd').collection('users');
+
+
+
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const option = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, option);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '8h' })
+            res.send({ result, token });
+        });
+
+        //user orders
+        app.get('/order', varifyJWT, async (req, res) => {
+            const email = req.query.email;
+            const decodedEmail = req.decoded.email;
+            if (email === decodedEmail) {
+                const query = { email: email };
+                const orders = await orderCollection.find(query).toArray();
+                res.send(orders);
+            }
+            else {
+                return res.status(403).send({ message: 'Forbidden access' })
+            }
+        })
+        //Delete API
+        app.delete('/order/:orderId', async (req, res) => {
+            const id = req.params.orderId;
+            const query = { _id: ObjectId(id) };
+            const result = await orderCollection.deleteOne(query);
+            res.send(result);
+        })
 
         //to get products form server
-        app.get('/products', async (req, res) => {
+        app.get('/product', async (req, res) => {
             const query = {};
             const cursor = productCollection.find(query);
             const products = await cursor.toArray();
             res.send(products);
         });
 
+        //to add products to server
+        app.post('/product', async (req, res) => {
+            const newProduct = req.body;
+            const result = await productCollection.insertOne(newProduct);
+            res.send(result);
+        });
+
+        //to get single product form server
+        app.get('/product/:productId', async (req, res) => {
+            const productId = req.params.productId;
+            const query = { _id: ObjectId(productId) };
+            const product = await productCollection.findOne(query);
+            res.send(product);
+        });
+
+        //order collection api
+        app.get('/order', async (req, res) => {
+            const query = {};
+            const orders = await orderCollection.find(query).toArray();
+            res.send(orders);
+        })
+        //Order POST API
+        app.post('/order', async (req, res) => {
+            const newOrder = req.body;
+            const result = await orderCollection.insertOne(newOrder);
+            res.send(result);
+        });
+
     }
     finally {
-
+        //final work
     }
 };
 run().catch(console.dir);
@@ -44,5 +124,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log("listening port", port);
+    console.log("listening to port", port);
 });
